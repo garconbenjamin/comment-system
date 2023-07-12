f3bb46ff60ba4775918ed00f70537944623b89d3
chinyu.hsu <chinyu.hsu@imersive.com>
2023-07-13 03:45:19 +0800

    Add dialog tip

--- src/components/CommentsDialog.tsx
+++ src/components/CommentsDialog.tsx
@@ -5,6 +5,9 @@ import moment from "moment";
 import type { Dialog } from "types";
 import { v4 as uuid } from "uuid";
 
+const Tip = () => (
+  <div className="w-4 h-4 bg-white absolute top-1/2 -left-2 transform rotate-45 shadow-lg" />
+);
 const CommentsDialog = (props: {
   currentDialog: Dialog;
   setDialogs: Dispatch<SetStateAction<Dialog[]>>;
@@ -65,73 +68,76 @@ const CommentsDialog = (props: {
   };
   return (
     <div
-      className="absolute bg-white rounded-lg overflow-hidden shadow-lg transform translate-x-12 -translate-y-1/2"
+      className="absolute transform translate-x-14 -translate-y-1/2"
       style={{
         top: position.y + y * zoom,
         left: position.x + x * zoom,
         width: 350,
       }}
     >
-      <div className="flex items-center border-b border-gray-300 py-2 px-4 gap-x-2">
-        {Object.entries(COLORS).map(([key, value]) => (
-          <div className="flex justify-center items-center" key={key}>
-            {currentDialog.color === key && (
-              <div className="mb-px absolute rounded-full w-7 h-7 border-2 border-gray-200" />
-            )}
-            <button
-              className="text-lg "
-              onClick={() =>
-                setCurrentDialog({
-                  ...currentDialog,
-                  color: key,
-                })
-              }
-            >
-              {value}
-            </button>
-          </div>
-        ))}
-        {Boolean(comments.length) && (
-          <button
-            className="btn bg-green-400 text-white px-2 "
-            onClick={handeResolve}
-          >
-            Resolve
-          </button>
-        )}
-
-        <button onClick={handleClose} className="ml-auto">
-          <MdClose color="black" size={20} />
-        </button>
-      </div>
-      {Boolean(comments.length) && (
-        <div className="px-4 py-2 border-b border-gray-300">
-          {comments.map((comment) => (
-            <div key={comment.id} className="">
-              <span className="font-bold pr-2">{comment.username} :</span>
-              <span>{comment.content}</span>
-              <span>{moment(comment.timestamp).format("YYYY/MM/DD")}</span>
-              <span>{moment(comment.timestamp).format("HH:mm:ss")}</span>
+      <Tip />
+      <div className="relative overflow-hidden bg-white rounded-lg shadow-lg">
+        <div className="flex items-center border-b border-gray-300 py-2 px-4 gap-x-2">
+          {Object.entries(COLORS).map(([key, value]) => (
+            <div className="flex justify-center items-center" key={key}>
+              {currentDialog.color === key && (
+                <div className="mb-px absolute rounded-full w-7 h-7 border-2 border-gray-200" />
+              )}
+              <button
+                className="text-lg "
+                onClick={() =>
+                  setCurrentDialog({
+                    ...currentDialog,
+                    color: key,
+                  })
+                }
+              >
+                {value}
+              </button>
             </div>
           ))}
+          {Boolean(comments.length) && (
+            <button
+              className="btn bg-green-400 text-white px-2 "
+              onClick={handeResolve}
+            >
+              Resolve
+            </button>
+          )}
+
+          <button onClick={handleClose} className="ml-auto">
+            <MdClose color="black" size={20} />
+          </button>
         </div>
-      )}
-      <div className="flex px-4 py-2">
-        <span className="font-bold pr-2">{username} :</span>
-        <input
-          type="text"
-          className="flex-1 border border-gray-300 rounded-md px-2"
-          placeholder="Add a comment"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button
-          disabled={!text}
-          className="pl-2 text-blue-500"
-          onClick={sendComment}
-        >
-          <MdSend size={20} />
-        </button>
+        {Boolean(comments.length) && (
+          <div className="px-4 py-2 border-b border-gray-300">
+            {comments.map((comment) => (
+              <div key={comment.id} className="">
+                <span className="font-bold pr-2">{comment.username} :</span>
+                <span>{comment.content}</span>
+                <span>{moment(comment.timestamp).format("YYYY/MM/DD")}</span>
+                <span>{moment(comment.timestamp).format("HH:mm:ss")}</span>
+              </div>
+            ))}
+          </div>
+        )}
+        <div className="flex px-4 py-2">
+          <span className="font-bold pr-2">{username} :</span>
+          <input
+            type="text"
+            className="flex-1 border border-gray-300 rounded-md px-2"
+            placeholder="Add a comment"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button
+            disabled={!text}
+            className="pl-2 text-blue-500"
+            onClick={sendComment}
+          >
+            <MdSend size={20} />
+          </button>
+        </div>
       </div>
     </div>
   );
