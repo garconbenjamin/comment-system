311f271152b8b98304bccac8020dcf7721810bee
Chin-yu Hsu <chinyu.hsu@imersive.com>
2023-07-14 17:25:58 +0800

    Create deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
+export default defineConfig(({ command }) => {
+  const config = { plugins: [react(), tsconfigPaths()] };
+  config.test = {
     globals: true,
     environment: "happy-dom",
-  },
+  };
+  if (command !== "serve") {
+    config.base = "/comment-system/";
+  }
+  return config;
 });
